c2743f46accee73d82f70451c1dba333458746ef
Sebbben <sebastian.jossang@gmail.com>
2024-10-04 21:39:28 +0200

    event page boilerplate

---
+++ app/pages/main/eventCalender/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+    title: "Event Calender",
+    description: "The event calender for cyb.no",
+  };
+  
+  export default function Layout({ children }) {
+    return children
+  }
---
+++ app/pages/main/eventCalender/page.js
@@ -0,0 +1,14 @@
+
+"use client";
+
+import { Box } from "@mui/material";
+import { useEffect, useState } from "react";
+
+export default function EventCalenderPage() {
+
+  
+  return (
+    <Box>
+    </Box>
+  );
+}
